409475babf2d3d08225c254777ff33ce831cb695
Emily <shootingstaretc@gmail.com>
2023-08-09 12:48:44 -0500

    added Ingredients List

--- part2/studio/src/components/Ingredients.js
+++ part2/studio/src/components/Ingredients.js
@@ -1,6 +1,21 @@
+import recipedata from "./recipe.json";
+
 function IngredientList() {
-   return;
- }
+  
+     const ingredientListItems= recipedata.map(recipe => {
+        return recipe.ingredients.map((ingredient, index) => {
+            return <li key={index}>{ingredient} </li>
+        });
+      });
+
+      return (
+        <div>
+          <h3>Ingredients</h3>
+          <li> {ingredientListItems} </li>
+        </div>
+      );
+      }
+      
  
  export default IngredientList;
  
